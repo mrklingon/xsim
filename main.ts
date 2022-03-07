bf41f79aaba69d1e860b9c5e2a399797726acfd6
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2022-03-07 16:59:02 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -17,6 +17,13 @@ function doTorp () {
 input.onButtonPressed(Button.AB, function () {
     doTorp()
 })
+function mkTies (quant: number) {
+    for (let index = 0; index < quant; index++) {
+        nuTie = game.createSprite(randint(0, 4), 0)
+        nuTie.turn(Direction.Right, 90)
+        Ties.push(nuTie)
+    }
+}
 input.onButtonPressed(Button.B, function () {
     xloc += 1
     if (xloc > 4) {
@@ -24,8 +31,32 @@ input.onButtonPressed(Button.B, function () {
     }
     xwing.set(LedSpriteProperty.X, xloc)
 })
+function killTie (tie: game.LedSprite) {
+    blah = Ties.removeAt(Ties.indexOf(tie))
+    blah.delete()
+    basic.pause(randint(200, 600))
+    mkTies(1)
+}
+let blah: game.LedSprite = null
+let nuTie: game.LedSprite = null
 let torp: game.LedSprite = null
+let Ties: game.LedSprite[] = []
 let xwing: game.LedSprite = null
 let xloc = 0
 xloc = 2
 xwing = game.createSprite(xloc, 4)
+Ties = []
+let tcount = 1
+basic.pause(1000)
+mkTies(tcount)
+doTorp()
+game.setLife(10)
+basic.forever(function () {
+    for (let value of Ties) {
+        value.move(1)
+        basic.pause(200)
+        if (4 == value.get(LedSpriteProperty.Y)) {
+            killTie(value)
+        }
+    }
+})
