ccc92bb52d5803bfba62c4d134c0b92423c7bc70
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2022-03-07 17:38:02 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+function doDroid (x: number) {
+    xwing.set(LedSpriteProperty.X, x)
+    xloc = x
+    doTorp()
+}
 input.onButtonPressed(Button.A, function () {
     xloc += -1
     if (xloc < 0) {
@@ -18,7 +23,7 @@ input.onButtonPressed(Button.AB, function () {
     doTorp()
 })
 function mkTies (quant: number) {
-    for (let index = 0; index < quant; index++) {
+    for (let index = 0; index < quant - Ties.length; index++) {
         nuTie = game.createSprite(randint(0, 4), 0)
         nuTie.turn(Direction.Right, 90)
         Ties.push(nuTie)
@@ -31,32 +36,78 @@ input.onButtonPressed(Button.B, function () {
     }
     xwing.set(LedSpriteProperty.X, xloc)
 })
+input.onGesture(Gesture.Shake, function () {
+    droid += 1
+    music.playTone(131, music.beat(BeatFraction.Whole))
+    if (droid > 1) {
+        droid = 0
+    }
+})
 function killTie (tie: game.LedSprite) {
     blah = Ties.removeAt(Ties.indexOf(tie))
     blah.delete()
     basic.pause(randint(200, 600))
-    mkTies(1)
+    mkTies(tcount)
 }
 let blah: game.LedSprite = null
 let nuTie: game.LedSprite = null
 let torp: game.LedSprite = null
+let tcount = 0
 let Ties: game.LedSprite[] = []
 let xwing: game.LedSprite = null
 let xloc = 0
+let droid = 0
+basic.showString("X-Sim!")
+images.createBigImage(`
+    . . # # # . # . . .
+    . . . # . . . . . #
+    # # # # # . . . . .
+    . . . # . . . . . .
+    . . # # # . . # . .
+    `).scrollImage(1, 200)
+images.createBigImage(`
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    `).scrollImage(1, 200)
+droid = 0
 xloc = 2
 xwing = game.createSprite(xloc, 4)
 Ties = []
-let tcount = 1
+tcount = 1
 basic.pause(1000)
 mkTies(tcount)
 doTorp()
-game.setLife(10)
+game.setLife(5)
 basic.forever(function () {
     for (let value of Ties) {
         value.move(1)
-        basic.pause(200)
+        basic.pause(333)
+        if (value.isTouching(torp)) {
+            killTie(value)
+            game.addScore(5)
+        }
+        if (value.isTouching(xwing)) {
+            game.removeLife(1)
+        }
         if (4 == value.get(LedSpriteProperty.Y)) {
             killTie(value)
         }
+    }
+})
+basic.forever(function () {
+    if (droid == 1) {
+        for (let index = 0; index <= 4; index++) {
+            doDroid(index)
+        }
+    }
+})
+basic.forever(function () {
+    basic.pause(2000)
+    tcount += 1
+    if (tcount > 4) {
+        tcount = 4
     }
 })
